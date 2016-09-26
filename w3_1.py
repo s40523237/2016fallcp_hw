138f63704748c4ecd6e2d9ea1fdee4113db7db22
s40523237 <40523237@gm.nfu.edu.tw>
2016-09-26 14:58:25 +0800

    Updated from Brython Server: 2016/9/26 下午2:58:24

--- w3_1.py
+++ w3_1.py
@@ -1,4 +1,10 @@
 print("第三週")
+
 thetext = input("Enter some text ")
+print ("This is what you entered:")
+print (thetext)
+
+# Note that \n within quote marks forces a new line to be printed
+thetext = input("Enter some text\n")
 print ("This is what you entered:")
 print (thetext)
